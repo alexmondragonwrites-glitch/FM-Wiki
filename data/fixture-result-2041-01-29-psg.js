56b943d97c9e3c3131d53c8377c6c056bcefd215
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-07-22 19:43:02 +0200

    Record PSG Champions League draw

---
+++ data/fixture-result-2041-01-29-psg.js
@@ -0,0 +1,7 @@
+(()=>{
+  window.FM_FIXTURES=window.FM_FIXTURES||[];
+  const result=['2041-01-29','20:00','Paris Saint-Germain','Heim','0:0','Ligaphase der UEFA Champions League',0];
+  const index=window.FM_FIXTURES.findIndex(item=>item[0]===result[0]&&item[2]===result[2]&&item[5]===result[5]);
+  if(index>=0)window.FM_FIXTURES[index]=result;
+  else window.FM_FIXTURES.push(result);
+})();
