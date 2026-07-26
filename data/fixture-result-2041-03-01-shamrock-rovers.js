74cc0b993059d9d7db1d743154bf0fbb3764cfff
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-07-26 14:34:27 +0200

    Register Shamrock Rovers 2-0 result

---
+++ data/fixture-result-2041-03-01-shamrock-rovers.js
@@ -0,0 +1,7 @@
+(()=>{
+  window.FM_FIXTURES=window.FM_FIXTURES||[];
+  const result=['2041-03-01','19:45','Shamrock Rovers','Heim','2:0','Premier Division',0];
+  const index=window.FM_FIXTURES.findIndex(item=>item[0]===result[0]&&item[2]===result[2]&&item[5]===result[5]);
+  if(index>=0)window.FM_FIXTURES[index]=result;
+  else window.FM_FIXTURES.push(result);
+})();
