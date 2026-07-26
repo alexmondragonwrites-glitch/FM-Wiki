06281635c919d49cf74c81368ecc7c6c7b7d74d3
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-07-26 19:22:18 +0200

    Register Shelbourne 1-5 league result

---
+++ data/fixture-result-2041-03-22-shelbourne.js
@@ -0,0 +1,7 @@
+(()=>{
+  window.FM_FIXTURES=window.FM_FIXTURES||[];
+  const result=['2041-03-22','19:45','Shelbourne','Auswärts','1:5','Premier Division',0];
+  const index=window.FM_FIXTURES.findIndex(item=>item[0]===result[0]&&item[2]===result[2]&&item[5]===result[5]);
+  if(index>=0)window.FM_FIXTURES[index]=result;
+  else window.FM_FIXTURES.push(result);
+})();
