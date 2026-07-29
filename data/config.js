007bf5a2962844a3c3ddc49b9e590de97d704d7e
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-07-29 18:51:26 +0200

    Point archive to Ireland win in Prague

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-03-27',
+    referenceDate:'2041-03-29',
     currentSeason:2040,
     nextSeason:2041,
-    updatedAt:'2041-03-27',
+    updatedAt:'2041-03-29',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2041-03-25',
+      players:'2041-03-29',
       transfers:'2041-02-16',
-      nationalTeam:'2041-03-27',
+      nationalTeam:'2041-03-29',
       clubs:'2041-03-25',
       matches:'2041-03-25',
       season:'2041-03-25',
-      news:'2041-03-27',
+      news:'2041-03-29',
       staff:'2040-11-02'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-03-27-czechia-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-03-29-czechia-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
