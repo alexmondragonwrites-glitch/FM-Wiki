e5fb85394dacd6401d26cd3ac4241b67678448da
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-07-30 19:16:30 +0200

    Point archive to Bayern quarterfinal triumph

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-04-13',
+    referenceDate:'2041-04-16',
     currentSeason:2040,
     nextSeason:2041,
-    updatedAt:'2041-04-13',
+    updatedAt:'2041-04-16',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2041-04-05',
+      players:'2041-04-16',
       transfers:'2041-02-16',
       nationalTeam:'2041-04-01',
-      clubs:'2041-04-13',
-      matches:'2041-04-13',
-      season:'2041-04-13',
-      news:'2041-04-13',
+      clubs:'2041-04-16',
+      matches:'2041-04-16',
+      season:'2041-04-16',
+      news:'2041-04-16',
       staff:'2040-11-02'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-04-13-sligo-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-04-16-bayern-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
