608330383b1d0d8b860549b051a12e5c2ec54d64
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-04 19:27:30 +0200

    Register Derry City derby result across archive domains

--- data/manifest-latest-results.js
+++ data/manifest-latest-results.js
@@ -11,7 +11,8 @@
     'data/transfer-rumours-2041-05-18.js',
     'data/vinicius-celta-cwc-draw-2041-05-18.js',
     'data/pol-munoz-transfer-confirmed-2041-05-20.js',
-    'data/st-patricks-result-2041-05-24.js'
+    'data/st-patricks-result-2041-05-24.js',
+    'data/derry-city-result-2041-05-31.js'
   ];
   const add=domain=>{
     const group=manifest[domain]||(manifest[domain]={current:[]});
