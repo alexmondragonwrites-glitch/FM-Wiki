3ea9014873b58db170d36a38c593f4e6434aec9d
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-04 06:35:55 +0200

    Register Vinícius transfer and Club World Cup draw

--- data/manifest-latest-results.js
+++ data/manifest-latest-results.js
@@ -8,7 +8,8 @@
     'data/ryan-flannigan-pressconference-2041-05-07.js',
     'data/athlone-town-result-2041-05-10.js',
     'data/shelbourne-result-2041-05-17.js',
-    'data/transfer-rumours-2041-05-18.js'
+    'data/transfer-rumours-2041-05-18.js',
+    'data/vinicius-celta-cwc-draw-2041-05-18.js'
   ];
   const add=domain=>{
     const group=manifest[domain]||(manifest[domain]={current:[]});
