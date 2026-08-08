3203090f8888c73e099a05b9afa67b1022d76459
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-08 19:19:43 +0200

    Feature Bayern reunion preview

--- data/config.js
+++ data/config.js
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-06-21-klub-wm-achtelfinale-bayern-auslosung'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-06-21-bayern-klub-wm-vorbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
