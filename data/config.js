72f20c724e90f9ff35442dd3dc16989934e58975
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-08 19:24:59 +0200

    Advance FM Wiki to Club World Cup upset round

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-06-21',
+    referenceDate:'2041-06-24',
     currentSeason:2041,
     nextSeason:2042,
-    updatedAt:'2041-06-21',
+    updatedAt:'2041-06-24',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
       players:'2041-06-21',
       transfers:'2041-06-20',
       nationalTeam:'2041-06-09',
-      clubs:'2041-06-21',
+      clubs:'2041-06-24',
       matches:'2041-06-21',
-      season:'2041-06-21',
-      news:'2041-06-21',
+      season:'2041-06-24',
+      news:'2041-06-24',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-06-21-bayern-klub-wm-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-06-24-klub-wm-achtelfinale-erste-ueberraschungen'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
