4cc3622476b44490b47ffbda23820cb245b63d01
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-11 19:31:46 +0200

    Register Athlone Town 5-1 result

--- data/manifest-latest-results.js
+++ data/manifest-latest-results.js
@@ -37,7 +37,8 @@
     'data/player-giacomo-papini-ireland-switch-2041-07-14.js',
     'data/shamrock-rovers-result-2041-07-15.js',
     'data/athenry-fai-cup-result-2041-07-18.js',
-    'data/cork-city-result-2041-07-22.js'
+    'data/cork-city-result-2041-07-22.js',
+    'data/athlone-town-result-2041-07-26.js'
   ];
   const add=(domain,entries=files)=>{
     const group=manifest[domain]||(manifest[domain]={current:[]});
