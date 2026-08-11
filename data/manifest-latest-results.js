e040a42e224f6a48266685759029bae7d26237da
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-11 06:14:21 +0200

    Register Athenry FAI Cup result

--- data/manifest-latest-results.js
+++ data/manifest-latest-results.js
@@ -35,7 +35,8 @@
     'data/player-emerson-escarcega-gold-cup-2041-07-10.js',
     'data/bohemian-result-2041-07-12.js',
     'data/player-giacomo-papini-ireland-switch-2041-07-14.js',
-    'data/shamrock-rovers-result-2041-07-15.js'
+    'data/shamrock-rovers-result-2041-07-15.js',
+    'data/athenry-fai-cup-result-2041-07-18.js'
   ];
   const add=(domain,entries=files)=>{
     const group=manifest[domain]||(manifest[domain]={current:[]});
