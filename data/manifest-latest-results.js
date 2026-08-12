a6a56ba30568f8b0bd93acb669053a8d6537bd27
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-12 20:15:30 +0200

    Register St Patrick's result and Escarcega debut

--- data/manifest-latest-results.js
+++ data/manifest-latest-results.js
@@ -45,7 +45,8 @@
     'data/transfer-window-closed-2041-08-01.js',
     'data/mitch-franke-sell-on-2041-08-02.js',
     'data/shelbourne-result-2041-08-02.js',
-    'data/player-emerson-escarcega-gold-cup-champion-2041-08-04.js'
+    'data/player-emerson-escarcega-gold-cup-champion-2041-08-04.js',
+    'data/st-patricks-result-2041-08-09.js'
   ];
   const add=(domain,entries=files)=>{
     const group=manifest[domain]||(manifest[domain]={current:[]});
