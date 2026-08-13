9c7e0a0c3a1d9bf6c8623fda5e4d00423918b884
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-13 19:16:46 +0200

    Advance wiki to Cork City comeback win

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-08-28',
+    referenceDate:'2041-08-30',
     currentSeason:2041,
     nextSeason:2042,
-    updatedAt:'2041-08-28',
+    updatedAt:'2041-08-30',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2041-08-24',
+      players:'2041-08-30',
       transfers:'2041-08-02',
       nationalTeam:'2041-08-04',
       clubs:'2041-07-07',
-      matches:'2041-08-24',
-      season:'2041-08-28',
-      news:'2041-08-28',
+      matches:'2041-08-30',
+      season:'2041-08-30',
+      news:'2041-08-30',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-08-24-sligo-rovers-liga-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-08-30-cork-city-liga-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
