5495b36bcf75b1fd041936ecb28765da421eabfe
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-14 17:58:49 +0200

    Advance wiki to Manchester United win

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-09-07',
+    referenceDate:'2041-09-10',
     currentSeason:2041,
     nextSeason:2042,
-    updatedAt:'2041-09-07',
+    updatedAt:'2041-09-10',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2041-09-06',
+      players:'2041-09-10',
       transfers:'2041-08-02',
       nationalTeam:'2041-08-04',
-      clubs:'2041-09-07',
-      matches:'2041-09-06',
-      season:'2041-09-07',
-      news:'2041-09-07',
+      clubs:'2041-09-10',
+      matches:'2041-09-10',
+      season:'2041-09-10',
+      news:'2041-09-10',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-09-07-manchester-united-ligaphase-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2041-09-10-manchester-united-ligaphase-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
