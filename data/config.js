0cf63d35d299a54e17b5380673bac2216b1b6966
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-15 16:33:50 +0200

    Update player data date for international duty

--- data/config.js
+++ data/config.js
@@ -8,7 +8,7 @@
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2041-10-03',
+      players:'2041-10-04',
       transfers:'2041-08-02',
       nationalTeam:'2041-10-04',
       clubs:'2041-09-18',
