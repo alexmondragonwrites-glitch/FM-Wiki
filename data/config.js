59049ceb87fe437e16ecd269ac795cf34f592a1e
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-19 19:53:05 +0200

    Set latest FM Wiki update to 2042 kit presentation

--- data/config.js
+++ data/config.js
@@ -1,10 +1,10 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2041-12-31',
+    referenceDate:'2042-01-08',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2041-12-31',
+    updatedAt:'2042-01-08',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
@@ -13,8 +13,8 @@
       nationalTeam:'2041-11-28',
       clubs:'2041-12-11',
       matches:'2041-12-11',
-      season:'2041-12-31',
-      news:'2041-12-31',
+      season:'2042-01-08',
+      news:'2042-01-08',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2041-12-31-finn-harps-kader-2042'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-01-08-finn-harps-kit-presentation'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
