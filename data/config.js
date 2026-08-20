8344a98d40d14a79f296a713461299edc9fb3a12
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-20 06:08:20 +0200

    Set Anderlecht preview as latest 2042 press item

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-01-10',
+    referenceDate:'2042-01-16',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-01-10',
+    updatedAt:'2042-01-16',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
       players:'2041-12-31',
       transfers:'2041-08-02',
       nationalTeam:'2042-01-10',
-      clubs:'2041-12-11',
+      clubs:'2042-01-16',
       matches:'2041-12-11',
-      season:'2042-01-10',
-      news:'2042-01-10',
+      season:'2042-01-16',
+      news:'2042-01-16',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-01-10-irland-brasilien-finalissima-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-01-16-finn-harps-anderlecht-vorbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
