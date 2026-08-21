4557c55d469700c82f951892e8990307be4adf92
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-21 19:09:29 +0200

    Advance wiki reference date for TV deal news

--- data/config.js
+++ data/config.js
@@ -1,10 +1,10 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-02-08',
+    referenceDate:'2042-02-09',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-02-08',
+    updatedAt:'2042-02-09',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
@@ -13,8 +13,8 @@
       nationalTeam:'2042-01-18',
       clubs:'2042-02-08',
       matches:'2042-02-08',
-      season:'2042-02-08',
-      news:'2042-02-08',
+      season:'2042-02-09',
+      news:'2042-02-09',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-02-08-finn-harps-derry-presidents-cup-report'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-02-09-premier-division-tv-deal'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
