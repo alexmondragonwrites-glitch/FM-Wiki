1c3ddfc51f2f23fae2a7407adde39f327d34263b
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-23 19:16:42 +0200

    Advance wiki to World Cup draw

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-03-29',
+    referenceDate:'2042-04-03',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-03-29',
+    updatedAt:'2042-04-03',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
       players:'2042-03-29',
       transfers:'2042-02-22',
-      nationalTeam:'2042-03-29',
+      nationalTeam:'2042-04-03',
       clubs:'2042-03-24',
       matches:'2042-03-29',
       season:'2042-03-24',
-      news:'2042-03-29',
+      news:'2042-04-03',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-03-29-irland-peru-3-2-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-04-03-world-cup-auslosung-irland-gruppe-k'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
