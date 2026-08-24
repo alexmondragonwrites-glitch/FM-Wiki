6e3359fd2d925a9311b132d6d3b17270292e05ba
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-24 19:27:48 +0200

    Advance wiki to Man City first-leg win

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-04-07',
+    referenceDate:'2042-04-09',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-04-07',
+    updatedAt:'2042-04-09',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-04-06',
+      players:'2042-04-09',
       transfers:'2042-02-22',
       nationalTeam:'2042-04-03',
-      clubs:'2042-04-07',
-      matches:'2042-04-06',
-      season:'2042-04-07',
-      news:'2042-04-07',
+      clubs:'2042-04-09',
+      matches:'2042-04-09',
+      season:'2042-04-09',
+      news:'2042-04-09',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-04-07-manchester-city-viertelfinale-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-04-09-finn-harps-man-city-1-0-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
