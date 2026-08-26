69b2da1fba6978767a892c2f4a5caa01260a484f
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-26 20:35:54 +0200

    Point press navigation to Champions League final preview

--- data/config.js
+++ data/config.js
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-06-finn-harps-barcelona-1-0-nv-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-05-06-champions-league-finale-finn-harps-man-utd'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
