72892f7e9f183927672f9b0837dcba027cc25f27
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-28 19:10:23 +0200

    Advance FM Wiki to Bohemian league result

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-05-09',
+    referenceDate:'2042-05-16',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-05-09',
+    updatedAt:'2042-05-16',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-05-09',
+      players:'2042-05-16',
       transfers:'2042-02-22',
       nationalTeam:'2042-04-03',
-      clubs:'2042-05-09',
-      matches:'2042-05-09',
-      season:'2042-05-09',
-      news:'2042-05-09',
+      clubs:'2042-05-16',
+      matches:'2042-05-16',
+      season:'2042-05-16',
+      news:'2042-05-16',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-09-finn-harps-athlone-town-5-1-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-05-16-bohemian-finn-harps-0-5-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
