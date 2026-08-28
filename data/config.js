ba19bda46bec6ec06d1f242f6a41b9b7c659449a
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-28 19:51:25 +0200

    Advance FM Wiki to Waterford home result

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-05-19',
+    referenceDate:'2042-05-23',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-05-19',
+    updatedAt:'2042-05-23',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-05-19',
+      players:'2042-05-23',
       transfers:'2042-02-22',
       nationalTeam:'2042-04-03',
-      clubs:'2042-05-19',
-      matches:'2042-05-19',
-      season:'2042-05-19',
-      news:'2042-05-19',
+      clubs:'2042-05-23',
+      matches:'2042-05-23',
+      season:'2042-05-23',
+      news:'2042-05-23',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-19-waterford-finn-harps-0-3-spielbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-05-23-finn-harps-waterford-3-1-spielbericht'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
