f9e1278fc10b03c2fdb85d5c86fdd196abbc9bbb
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-28 19:51:11 +0200

    Register Waterford home result in 2042 manifest

--- data/manifest-2042.js
+++ data/manifest-2042.js
@@ -78,4 +78,7 @@
 
   const waterfordResultMay='data/waterford-result-2042-05-19.js';
   ['players','matches','fixtures','clubs','archive','league','press','news'].forEach(key=>add(key,waterfordResultMay));
+
+  const waterfordHomeResultMay='data/waterford-result-2042-05-23.js';
+  ['players','matches','fixtures','clubs','archive','league','press','news'].forEach(key=>add(key,waterfordHomeResultMay));
 })();
