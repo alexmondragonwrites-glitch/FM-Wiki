8ea7ce5a7dce0a8636c12e9d9b8fb533f4165b4e
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-29 19:51:53 +0200

    Set Ireland Cameroon friendly as latest World Cup update

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-05-31',
+    referenceDate:'2042-06-02',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-05-31',
+    updatedAt:'2042-06-02',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-05-31',
+      players:'2042-06-02',
       transfers:'2042-02-22',
-      nationalTeam:'2042-05-31',
+      nationalTeam:'2042-06-02',
       clubs:'2042-05-31',
-      matches:'2042-05-31',
+      matches:'2042-06-02',
       season:'2042-05-31',
-      news:'2042-05-31',
+      news:'2042-06-02',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-31-champions-league-final-man-utd-finn-harps-1-0'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-06-02-irland-kamerun-4-0-wm-test'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
