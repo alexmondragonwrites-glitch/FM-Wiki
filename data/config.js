1c82a5849f94699962102b7956480d7eaa0698dc
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-29 20:24:58 +0200

    Move wiki focus to Ireland World Cup final warm-up

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-06-02',
+    referenceDate:'2042-06-08',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-06-02',
+    updatedAt:'2042-06-08',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-06-02',
+      players:'2042-06-08',
       transfers:'2042-02-22',
-      nationalTeam:'2042-06-02',
+      nationalTeam:'2042-06-08',
       clubs:'2042-05-31',
-      matches:'2042-06-02',
+      matches:'2042-06-08',
       season:'2042-05-31',
-      news:'2042-06-02',
+      news:'2042-06-08',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-06-02-irland-kamerun-4-0-wm-test'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-06-08-irland-dr-kongo-9-1-letzter-wm-test'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
