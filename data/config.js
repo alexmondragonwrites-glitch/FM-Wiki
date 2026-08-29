d2c512e00eed2b4bf34ac5980863acb3fe283609
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-29 19:06:24 +0200

    Set Champions League final scouting as latest press focus

--- data/config.js
+++ data/config.js
@@ -13,7 +13,7 @@
       nationalTeam:'2042-05-25',
       clubs:'2042-05-25',
       matches:'2042-05-23',
-      season:'2042-05-23',
+      season:'2042-05-25',
       news:'2042-05-25',
       staff:'2041-06-17'
     }),
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-25-pol-munoz-portrait-von-coventry-zur-wm'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-05-25-champions-league-final-manchester-united-scouting'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
