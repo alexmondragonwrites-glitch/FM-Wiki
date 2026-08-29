134200c13a3d1652b76ea094a931109ed89eb3ff
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-29 19:26:50 +0200

    Set Champions League final as current 2042 wiki focus

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-05-25',
+    referenceDate:'2042-05-31',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-05-25',
+    updatedAt:'2042-05-31',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-05-25',
+      players:'2042-05-31',
       transfers:'2042-02-22',
-      nationalTeam:'2042-05-25',
-      clubs:'2042-05-25',
-      matches:'2042-05-23',
-      season:'2042-05-25',
-      news:'2042-05-25',
+      nationalTeam:'2042-05-31',
+      clubs:'2042-05-31',
+      matches:'2042-05-31',
+      season:'2042-05-31',
+      news:'2042-05-31',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-05-25-champions-league-final-manchester-united-scouting'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-05-31-champions-league-final-man-utd-finn-harps-1-0'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
