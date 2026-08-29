f2bd2a6eb4c47c3f39fd61248084482a8783a978
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-29 20:31:13 +0200

    Correct June 7 World Cup prep date and press focus

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-06-08',
+    referenceDate:'2042-06-07',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-06-08',
+    updatedAt:'2042-06-07',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
-      players:'2042-06-08',
+      players:'2042-06-07',
       transfers:'2042-02-22',
-      nationalTeam:'2042-06-08',
+      nationalTeam:'2042-06-07',
       clubs:'2042-05-31',
-      matches:'2042-06-08',
+      matches:'2042-06-07',
       season:'2042-05-31',
-      news:'2042-06-08',
+      news:'2042-06-07',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-06-08-irland-dr-kongo-9-1-letzter-wm-test'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-06-07-harps-international-roundup-world-cup-prep'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
