eb4c517686f423d9e2291638f4550609127d6a6f
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-30 17:15:57 +0200

    Set Ireland 4-0 China as current World Cup focus

--- data/config.js
+++ data/config.js
@@ -12,7 +12,7 @@
       transfers:'2042-02-22',
       nationalTeam:'2042-06-15',
       clubs:'2042-05-31',
-      matches:'2042-06-07',
+      matches:'2042-06-15',
       season:'2042-05-31',
       news:'2042-06-15',
       staff:'2041-06-17'
@@ -22,12 +22,12 @@
       {id:'squad',label:'Kader',href:'kader.html'},
       {id:'planning',label:'Planung',href:'kaderplanung.html'},
       {id:'archive',label:'Archiv',href:'kaderarchiv.html?year=2041'},
-      {id:'players',label:'Spieler',href:'spieler.html?id=paulo-henrique'},
+      {id:'players',label:'Spieler',href:'spieler.html?id=ben-barry'},
       {id:'ireland',label:'Nationalteam',href:'nationalteam.html'},
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-06-15-irland-china-wm-vorbericht'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-06-15-china-irland-0-4-wm-auftakt'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
