f4bdf146aac2b32965fc94c41846874077c068d6
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-08-30 18:27:45 +0200

    Advance wiki focus to World Cup results on 20 June 2042

--- data/config.js
+++ data/config.js
@@ -1,20 +1,20 @@
 (()=>{
   const config={
     schemaVersion:2,
-    referenceDate:'2042-06-19',
+    referenceDate:'2042-06-20',
     currentSeason:2042,
     nextSeason:2043,
-    updatedAt:'2042-06-19',
+    updatedAt:'2042-06-20',
     locale:'de-DE',
     siteName:'Finn Harps FM Wiki',
     dataDates:Object.freeze({
       players:'2042-06-19',
       transfers:'2042-02-22',
-      nationalTeam:'2042-06-19',
+      nationalTeam:'2042-06-20',
       clubs:'2042-05-31',
       matches:'2042-06-19',
       season:'2042-05-31',
-      news:'2042-06-19',
+      news:'2042-06-20',
       staff:'2041-06-17'
     }),
     navigation:[
@@ -27,7 +27,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-06-19-irland-suedafrika-1-0-barry'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-06-20-world-cup-groups-a-c-final-matchday'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
