788a909a87f721e4401aac33386bde53f89b0b03
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-09-04 19:02:43 +0200

    Register Belgium-Japan and Serbia-South Korea World Cup results

--- data/manifest-2042.js
+++ data/manifest-2042.js
@@ -174,4 +174,7 @@
 
   const worldCupRoundOf16July3='data/world-cup-round-of-16-results-austria-scouting-2042-07-03.js';
   ['players','nationalTeam','archive','press','news'].forEach(key=>add(key,worldCupRoundOf16July3));
+
+  const worldCupRoundOf16July3Late='data/world-cup-round-of-16-results-belgium-korea-2042-07-03.js';
+  ['players','nationalTeam','archive','press','news'].forEach(key=>add(key,worldCupRoundOf16July3Late));
 })();
