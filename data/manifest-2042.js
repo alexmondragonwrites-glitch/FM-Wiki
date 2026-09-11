2141a3800d7b4ec13f6920260b72528cacd84688
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-09-11 06:10:36 +0200

    Register Netherlands World Cup semifinal scouting

--- data/manifest-2042.js
+++ data/manifest-2042.js
@@ -69,7 +69,8 @@
     [['players','transfers','clubs','archive','league','press','news'],'data/alejandro-lopez-signing-2042-07-05.js'],
     [['players','matches','nationalTeam','archive','press','news'],'data/ireland-austria-result-quarterfinals-2042-07-05.js'],
     [['players','nationalTeam','archive','press','news'],'data/world-cup-quarterfinal-results-2042-07-07.js'],
-    [['players','matches','nationalTeam','archive','press','news'],'data/world-cup-quarterfinal-results-2042-07-09.js']
+    [['players','matches','nationalTeam','archive','press','news'],'data/world-cup-quarterfinal-results-2042-07-09.js'],
+    [['nationalTeam','archive','press','news'],'data/netherlands-world-cup-semifinal-scouting-2042-07-11.js']
   ];
 
   entries.forEach(([keys,path])=>keys.forEach(key=>add(key,path)));
