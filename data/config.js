fbc74b0081904bc7a0f2c507573127b606d1498b
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-09-11 18:23:30 +0200

    Point press navigation to World Cup 2042 aftermath

--- data/config.js
+++ data/config.js
@@ -18,7 +18,7 @@
       {id:'season',label:'Saison',href:'saison.html'},
       {id:'matches',label:'Spiele',href:'spiele.html'},
       {id:'clubs',label:'Klubs',href:'klubs.html'},
-      {id:'press',label:'Presse',href:'presse.html?id=2042-07-20-mexico-world-champion-ireland-final-heartbreak'},
+      {id:'press',label:'Presse',href:'presse.html?id=2042-07-20-world-cup-2042-aftermath-awards-reactions'},
       {id:'club',label:'Verein',href:'verein.html'},
       {id:'staff',label:'Mitarbeiter',href:'mitarbeiter.html'},
       {id:'docs',label:'Dokumentation',href:'dokumentation.html'}
