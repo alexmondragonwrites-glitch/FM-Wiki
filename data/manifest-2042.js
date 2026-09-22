d5461f9b25a7053b933b403d61cf6be14b6826c8
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-09-22 06:10:17 +0200

    Register Benfica 1-0 Champions League result

--- data/manifest-2042.js
+++ data/manifest-2042.js
@@ -96,7 +96,8 @@
     [['clubs','fixtures','archive','league','press','news'],'data/inter-preview-2042-09-08.js'],
     [['players','matches','fixtures','clubs','archive','league','press','news'],'data/inter-result-2042-09-09.js'],
     [['clubs','fixtures','archive','league','press','news'],'data/benfica-preview-2042-09-10.js'],
-    [['clubs','archive','league','press','news'],'data/champions-league-matchday1-wednesday-2042-09-10.js']
+    [['clubs','archive','league','press','news'],'data/champions-league-matchday1-wednesday-2042-09-10.js'],
+    [['players','matches','fixtures','clubs','archive','league','press','news'],'data/benfica-result-2042-09-16.js']
   ];
 
   entries.forEach(([keys,path])=>keys.forEach(key=>add(key,path)));
