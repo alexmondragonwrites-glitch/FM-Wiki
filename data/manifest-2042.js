868aa0582b81c1a4ab74e8de062a91d9834d42d5
alexmondragonwrites-glitch <alexmondragonwrites@gmail.com>
2026-09-24 06:06:50 +0200

    Register Derry City FAI Cup quarterfinal result

--- data/manifest-2042.js
+++ data/manifest-2042.js
@@ -98,7 +98,8 @@
     [['clubs','fixtures','archive','league','press','news'],'data/benfica-preview-2042-09-10.js'],
     [['clubs','archive','league','press','news'],'data/champions-league-matchday1-wednesday-2042-09-10.js'],
     [['players','matches','fixtures','clubs','archive','league','press','news'],'data/benfica-result-2042-09-16.js'],
-    [['archive','league','press','news'],'data/champions-league-matchday2-wednesday-2042-09-17.js']
+    [['archive','league','press','news'],'data/champions-league-matchday2-wednesday-2042-09-17.js'],
+    [['players','matches','fixtures','clubs','archive','press','news'],'data/derry-city-fai-cup-quarterfinal-result-2042-09-21.js']
   ];
 
   entries.forEach(([keys,path])=>keys.forEach(key=>add(key,path)));
